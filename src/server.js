fa8c50acd34de6d9b0510967e410889270191de0
NguyenDucAnh2610 <97513336+NguyenDucAnh2610@users.noreply.github.com>
2022-07-09 18:39:45 +0700

    first deploy

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("./database/db.json");
 const middlewares = jsonServer.defaults({
-  static: "../build",
+  static: "./build",
 });
 const port = process.env.PORT || 4000;
 server.db = router.db;
